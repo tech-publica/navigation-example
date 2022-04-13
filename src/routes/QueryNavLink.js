37c3fd68700d95dde3f979043972a91c4bfa6e25
ric <riccardo.at.work@gmail.com>
2022-04-13 11:14:24 +0200

    initial commit

---
+++ src/routes/QueryNavLink.js
@@ -0,0 +1,7 @@
+import { useLocation, NavLink } from "react-router-dom";
+
+function QueryNavLink({ to, ...props }) {
+  let location = useLocation();
+  return <NavLink to={to + location.search} {...props} />;
+}
+export default QueryNavLink;
